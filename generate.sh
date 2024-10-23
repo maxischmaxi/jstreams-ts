1fed2ddb2e15a91b6c804dcddd8a42d3d11ff6e0
maxischmaxi <max@jeschek.dev>
2024-10-23 11:45:24 +0200

    Update protos

--- generate.sh
+++ generate.sh
@@ -19,6 +19,23 @@ rm -rf jstreams-server
 
 npx buf generate
 
+if [ ! -f package.json ]; then
+  echo "package.json nicht gefunden!"
+  exit 1
+fi
+
+current_version=$(grep '"version"' package.json | sed -E 's/.*"([0-9]+\.[0-9]+\.[0-9]+)".*/\1/')
+
+if [ -z "$current_version" ]; then
+  echo "Version konnte nicht gefunden werden!"
+  exit 1
+fi
+
+IFS='.' read -r -a version_parts <<< "$current_version"
+new_patch=$((version_parts[2] + 1))
+new_version="${version_parts[0]}.${version_parts[1]}.$new_patch"
+sed -i '' "s/\"version\": \"$current_version\"/\"version\": \"$new_version\"/" package.json
+
 git add .
 
 git commit -m "Update protos"
